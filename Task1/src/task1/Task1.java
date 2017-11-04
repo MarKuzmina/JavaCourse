3876234488cbedaf5887f33cdc28228ddaf5fa88
MariKuzmina <marina.kuzmina96@inbox.ru>
2017-11-04 22:58:16 +0300

    Task1 done

---
+++ Task1/src/task1/Task1.java
@@ -0,0 +1,12 @@
+package task1;
+
+/**
+ *
+ * @author Marina
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.print(Integer.parseInt(args [0]) + Integer.parseInt(args [1]));
+    }
+    
+}
